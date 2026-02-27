4485067c1bb40d8ac801620edcaed2710a92dab7
Bekvision <asad@student.pdp.universit>
2026-02-27 17:24:04 +0500

    new commit

--- alembic/versions/05a9efcb7e24_add_stock_count_to_products.py
+++ alembic/versions/05a9efcb7e24_add_stock_count_to_products.py
@@ -19,9 +19,31 @@ branch_labels = None
 depends_on = None
 
 
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy import text
+
 def upgrade():
-    op.add_column("products", sa.Column("stock_count", sa.Integer(), nullable=False, server_default="0"))
-    op.alter_column("products", "stock_count", server_default=None)
+    bind = op.get_bind()
+    exists = bind.execute(text("""
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name='products' AND column_name='stock_count'
+    """)).first()
+
+    if not exists:
+        op.add_column(
+            "products",
+            sa.Column("stock_count", sa.Integer(), nullable=False, server_default="0")
+        )
 
 def downgrade():
-    op.drop_column("products", "stock_count")
+    bind = op.get_bind()
+    exists = bind.execute(text("""
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name='products' AND column_name='stock_count'
+    """)).first()
+
+    if exists:
+        op.drop_column("products", "stock_count")
