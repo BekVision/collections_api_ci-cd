7700a215017b560f4c5eaf44ef2f5f237bdc8a6a
Bekvision <asad@student.pdp.universit>
2026-02-27 17:15:37 +0500

    alembic: merge multiple heads

---
+++ alembic/versions/6ae3c0a09f99_merge_heads.py
@@ -0,0 +1,26 @@
+"""merge heads
+
+Revision ID: 6ae3c0a09f99
+Revises: 05a9efcb7e24, 9a787c282438
+Create Date: 2026-02-27 17:15:23.662243
+
+"""
+from __future__ import annotations
+
+from alembic import op
+import sqlalchemy as sa
+
+
+
+revision = '6ae3c0a09f99'
+down_revision = ('05a9efcb7e24', '9a787c282438')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
